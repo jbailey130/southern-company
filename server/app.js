45da134512fe579a69dafb52953b535fdc0cdf1d
jbailey130 <jbailey@icovian.com>
2017-02-25 17:28:23 -0500

    more favicon stuff

--- server/app.js
+++ server/app.js
@@ -7,4 +7,4 @@ app.use(express.static(staticPath));
 app.get('/*', (req, res) => res.redirect(staticPath + '/index.html'))
 
 
-app.listen(3001, () => console.log('listening on server port 3001'));
+app.listen(80, () => console.log('listening on server port 80'));
